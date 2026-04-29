e6fcc5152c34d0c7567b653d2f8be87d42dceb34
Jakob Aschauer @Unibo <jakob.aschauer@studio.unibo.it>
2026-04-29 17:36:43 +0200

    Add is_positive function to check number sign

---
+++ R/is_positive.R
@@ -0,0 +1,11 @@
+is_positive <- function(x) {
+  if (x>0) {
+    print("The number is positive")
+    }
+  else if (x<0) {
+    print("The number is negative")
+    }
+  else {
+    print("Zero")
+    }
+  }
